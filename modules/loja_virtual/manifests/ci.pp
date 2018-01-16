f6c0784eb571899b3582fa86f538bb6cce14a37a
Bruno Leite Alves <brunoleitealves@gmail.com>
2018-01-16 16:24:57 -0500

    Automatiza criação de job no ci

--- modules/loja_virtual/manifests/ci.pp
+++ modules/loja_virtual/manifests/ci.pp
@@ -1,7 +1,5 @@
-class loja_virtual::ci {
-  include loja_virtual
-  
-  apt::ppa { 'ppa:openjdk-r/ppa': }
+class loja_virtual::ci inherits loja_virtual {
+  apt::ppa { 'ppa:openjdk-r/ppa': }  
 
   package { ['git', 'maven2', 'openjdk-6-jdk', 'openjdk-8-jdk']:
     ensure => "installed",
@@ -43,5 +41,42 @@ class loja_virtual::ci {
   ]
 
   jenkins::plugin { $plugins: }
+
+  file { '/var/lib/jenkins/hudson.tasks.Maven.xml':
+    mode    => '0644',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    source  => 'puppet:///modules/loja_virtual/hudson.tasks.Maven.xml',
+    require => Class['jenkins::package'],
+    notify  => Service['jenkins'],
+  }
+
+  $job_structure = [
+    '/var/lib/jenkins/jobs/',
+    '/var/lib/jenkins/jobs/loja-virtual-devops'
+  ]
+  $git_repository = 'https://github.com/brunoleite/loja-virtual-devops.git'
+  $git_poll_interval = '* * * * *'
+  $maven_goal = 'install'
+  $archive_artifacts = 'combined/target/*.war'
+
+  file { $job_structure[1]:
+    ensure => 'directory',
+    owner  => 'jenkins',
+    group  => 'jenkins',
+    require => [
+        File['/var/lib/jenkins/jobs/'],
+        Class['jenkins::package']
+      ],
+  }
+  
+  file { "${job_structure[1]}/config.xml":
+    mode    => '0644',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    content => template('loja_virtual/config.xml'),
+    require => File[$job_structure],
+    notify  => Service['jenkins'],
+  }
 }
 
